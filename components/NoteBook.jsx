5f4aae75e717526081dd853fe97aca68e79dee30
ydk1204 <ejrbdi@naver.com>
2023-07-04 19:11:51 +0900

    방안3-7

--- components/NoteBook.jsx
+++ components/NoteBook.jsx
@@ -3,6 +3,7 @@ const Link = dynamic(import("next/link"));
 
 import { allPosts } from "contentlayer/generated";
 // import Link from "next/link";
+import { useState } from "react";
 
 const colors = [
   { key: "html", color: "red" },
@@ -13,18 +14,19 @@ const colors = [
 
 const NoteBook = ({ title, count }) => {
   const getColor = colors.filter((a) => a.key === title)[0].color;
+  const [slug, setSlug] = useState("");
   // const customColor = `bg-${getColor}-500`;
 
-  const slugs = allPosts
-    .filter((a) => a.note === title)
-    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
+  // const slugs = allPosts
+  //   .filter((a) => a.note === title)
+  //   .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
 
-  const slug = slugs[0]._raw.flattenedPath;
+  // const slug = slugs[0]._raw.flattenedPath;
+  getSlug(title).then((res) => setSlug(res));
 
   return (
     <Link
       href={`/study/${title}/${slug}`}
-      key={title}
       className="w-60 h-80 bg-slate-700 rounded-md"
     >
       <div
@@ -37,6 +39,15 @@ const NoteBook = ({ title, count }) => {
       </div>
     </Link>
   );
+};
+
+export const getSlug = async (title) => {
+  const slugs = allPosts
+    .filter((a) => a.note === title)
+    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
+
+  const slug = slugs[0]._raw.flattenedPath;
+  return slug;
 };
 
 export default NoteBook;
