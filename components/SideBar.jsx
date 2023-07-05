d6d88da707e13a452aba662ced62e73da28d33fc
ydk1204 <ejrbdi@naver.com>
2023-07-05 20:31:56 +0900

    toc header list 클릭 시 이동 로직 추가 - 추후 변경이 필요하다고 생각됨

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,25 +1,42 @@
 import { useState, useEffect } from "react";
 import getIntersectionObserver from "../lib/observer";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const SideBar = ({ post }) => {
   const [currentId, setCurrentId] = useState("");
   const [headingEls, setHeadingEls] = useState([]);
   const [headingContents, setHeadingContents] = useState([]);
 
+  // header에 id 값을 지정 후 Link 클릭 시 지정한 id값이 초기화 되어 route값이 변경될 때마다 useEffect로 id 할당
+  const router = useRouter();
+
   useEffect(() => {
     let contents = [];
-    const observer = getIntersectionObserver(setCurrentId);
     const headingElements = Array.from(document.querySelectorAll("h2, h3"));
+
+    const visibleHeaders = Array.from(
+      { length: headingElements.length },
+      () => false
+    );
+
     headingElements.forEach((a) => {
-      contents.push(a.innerHTML);
+      a.id = a.innerHTML;
+      contents.push(a.id);
     });
     setHeadingContents(contents);
 
     setHeadingEls(headingElements);
 
+    const observer = getIntersectionObserver(
+      setCurrentId,
+      visibleHeaders,
+      contents
+    );
     headingElements.map((header) => {
       observer.observe(header);
     });
-  }, [post]);
+  }, [post, router.asPath]);
 
   return (
     <>
@@ -27,9 +44,19 @@ const SideBar = ({ post }) => {
         {headingContents.map((content, idx) => (
           <li
             key={idx}
-            className={`${content === currentId && "text-green-600"}`}
+            className={`${
+              content === currentId && "text-green-600 duration-150"
+            } ${
+              headingEls[idx]?.nodeName === "H2" ? "text-lg" : "ml-4 text-sm"
+            }`}
           >
-            {content}
+            <Link
+              href={`/${post.tag}/${post.note}/${post._raw.flattenedPath}/#${content}`}
+              passHref
+              legacyBehavior
+            >
+              <a>{content}</a>
+            </Link>
           </li>
         ))}
       </aside>
