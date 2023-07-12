81814d1e1e322b813a9ca7cf65a70326683f08b4
ydk1204 <ejrbdi@naver.com>
2023-07-12 21:44:44 +0900

    텍스트 이펙트 일정 범위 벗어나면 멈추기

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,15 +3,41 @@ import Image from "next/image";
 import RecentPosts from "../components/RecentPosts";
 import metadata from "../data/metadata";
 import TypeIt from "typeit-react";
+import { useState, useEffect, useRef } from "react";
 
 import { allPosts } from "contentlayer/generated";
-import { InferGetStaticPropsType } from "next";
+
+const options = {
+  threshold: [0, 0.5, 1],
+};
 
 const Home = ({ posts }) => {
+  const [instance, setInstance] = useState(null);
+
+  // 텍스트 이펙트를 주는 object가 뷰포트에서 대략 절반이상 안 보이면 정지(freeze),
+  // 범위 내 들어오면 다시 실행(unfreeze)
+  const callback = (entries) => {
+    instance !== null &&
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0.4) {
+          instance.unfreeze();
+        } else {
+          instance.freeze();
+        }
+      });
+  };
+
+  // IntersectionObserver
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, options);
+    const box = document.querySelector(".main-box");
+    observer.observe(box);
+  }, [instance]);
+
   return (
     <Conainer>
       <div className={`my-5 w-full`}>
-        <div className={`relative`}>
+        <div className={`relative main-box`}>
           <Image
             src={`/home.png`}
             alt="대표 이미지"
@@ -30,19 +56,20 @@ const Home = ({ posts }) => {
                   instance
                     .pause(300)
                     .move(-7)
-                    .type("간단하게 ") // 간단하게 생각하고| 살기
+                    .type("간단하게 ")
                     .move(4)
                     .pause(300)
                     .type(", 편하게")
                     .move(null, { to: "END" })
-                    .pause(1000)
-                    .delete()
-                    .go();
+                    .pause(1000);
+                  setInstance(instance);
                   return instance;
                 }}
                 options={{
+                  cursor: false,
+                  waitUntilVisible: true,
                   loop: true,
-                  speed: 50,
+                  speed: 100,
                 }}
               >
                 생각하고 살기
