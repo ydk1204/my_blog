9f46dd4aa3883d0036ace8459cbfd6c3a594d4e4
ydk1204 <ejrbdi@naver.com>
2024-01-09 23:04:04 +0900

    IoC, DIP, DI 포스팅

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -4,18 +4,19 @@ import game__gameMdx from './game__game포스팅.mdx.json' assert { type: 'json'
 import study__algorithmStudy__JavaScript_11724Mdx from './study__algorithm-study__백준-JavaScript-11724.mdx.json' assert { type: 'json' }
 import study__algorithmStudy__JavaScript_1260Mdx from './study__algorithm-study__백준-JavaScript-1260.mdx.json' assert { type: 'json' }
 import study__algorithmStudy__JavaScript_2579Mdx from './study__algorithm-study__백준-JavaScript-2579.mdx.json' assert { type: 'json' }
-import study__csStudy__Mdx from './study__cs-study__디자인패턴.mdx.json' assert { type: 'json' }
-import Post5 from './study__cs-study__싱글톤패턴.mdx.json' assert { type: 'json' }
-import Post6 from './study__cs-study__이터레이터패턴.mdx.json' assert { type: 'json' }
-import Post7 from './study__cs-study__팩토리패턴.mdx.json' assert { type: 'json' }
-import Post8 from './study__cs-study__프레임워크와-라이브러리.mdx.json' assert { type: 'json' }
-import study__htmlStudy__seoMdx from './study__html-study__SEO란.mdx.json' assert { type: 'json' }
-import study__htmlStudy__htmlMdx from './study__html-study__html초기화.mdx.json' assert { type: 'json' }
+import study__jsStudy__jsMdx from './study__js-study__js초기화.mdx.json' assert { type: 'json' }
 import study__cssStudy__cssMdx from './study__css-study__css초기화.mdx.json' assert { type: 'json' }
 import study__cssStudy__JsxCssMdx from './study__css-study__리액트-JSX에서-CSS-변수사용.mdx.json' assert { type: 'json' }
+import study__csStudy__iocDipDiMdx from './study__cs-study__IOC-DIP-DI.mdx.json' assert { type: 'json' }
+import study__csStudy__Mdx from './study__cs-study__디자인패턴.mdx.json' assert { type: 'json' }
+import Post9 from './study__cs-study__싱글톤패턴.mdx.json' assert { type: 'json' }
+import Post10 from './study__cs-study__이터레이터패턴.mdx.json' assert { type: 'json' }
+import Post11 from './study__cs-study__팩토리패턴.mdx.json' assert { type: 'json' }
+import Post12 from './study__cs-study__프레임워크와-라이브러리.mdx.json' assert { type: 'json' }
 import study__reactStudy__reactJsxMdx from './study__react-study__React와JSX.mdx.json' assert { type: 'json' }
 import study__reactStudy__Mdx from './study__react-study__리액트와사이드이펙트.mdx.json' assert { type: 'json' }
-import study__jsStudy__jsMdx from './study__js-study__js초기화.mdx.json' assert { type: 'json' }
+import study__htmlStudy__seoMdx from './study__html-study__SEO란.mdx.json' assert { type: 'json' }
+import study__htmlStudy__htmlMdx from './study__html-study__html초기화.mdx.json' assert { type: 'json' }
 import projects__projectsMdx from './projects__projects포스팅.mdx.json' assert { type: 'json' }
 
-export const allPosts = [game__gameMdx, study__algorithmStudy__JavaScript_11724Mdx, study__algorithmStudy__JavaScript_1260Mdx, study__algorithmStudy__JavaScript_2579Mdx, study__csStudy__Mdx, Post5, Post6, Post7, Post8, study__htmlStudy__seoMdx, study__htmlStudy__htmlMdx, study__cssStudy__cssMdx, study__cssStudy__JsxCssMdx, study__reactStudy__reactJsxMdx, study__reactStudy__Mdx, study__jsStudy__jsMdx, projects__projectsMdx]
+export const allPosts = [game__gameMdx, study__algorithmStudy__JavaScript_11724Mdx, study__algorithmStudy__JavaScript_1260Mdx, study__algorithmStudy__JavaScript_2579Mdx, study__jsStudy__jsMdx, study__cssStudy__cssMdx, study__cssStudy__JsxCssMdx, study__csStudy__iocDipDiMdx, study__csStudy__Mdx, Post9, Post10, Post11, Post12, study__reactStudy__reactJsxMdx, study__reactStudy__Mdx, study__htmlStudy__seoMdx, study__htmlStudy__htmlMdx, projects__projectsMdx]
